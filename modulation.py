5de00d944a24007eea3cfce8eece5c1d9835fafb
Aliaksei <alexx333by@protonmail.com>
2019-11-07 23:45:30 +0300

    Frequency modulation implemented

--- modulation.py
+++ modulation.py
@@ -8,3 +8,11 @@ def amplitude_modulation(carrier_wave, data_wave, depth=1):
     normalized_data = __normalize_wave(data_wave, 1)
     modulated_wave_length = min(len(normalized_carrier), len(normalized_data))
     return [(1 + depth * normalized_data[i]) * normalized_carrier[i] for i in range(modulated_wave_length)]
+
+
+def frequency_modulation(carrier_signal_generator, data_wave, carrier_frequency, frequency_deviation):
+    assert frequency_deviation < carrier_frequency, "Frequency deviation should be bigger than carrier frequency"
+    normalized_data = __normalize_wave(data_wave, 1)
+
+    return [carrier_signal_generator(i, carrier_frequency + normalized_data[i] * frequency_deviation)
+            for i in range(len(normalized_data))]
